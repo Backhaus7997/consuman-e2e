cce106058a3e73be0603dedd6141a27b552392ac
Martin Backhaus <tinchoignacio33@gmail.com>
2025-11-13 12:48:02 -0300

    changes v2

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test.setTimeout(120000);
 
 async function handleLogin(page) {
-  await page.getByLabel('Email').fill('ovillalon@consuman.com');
+  await page.getByLabel('Email').fill('admin@consuman.com');
   await page.waitForTimeout(2000);
 
-  await page.getByLabel('Password').fill('Villalon1920#');
+  await page.getByLabel('Password').fill('123');
   await page.waitForTimeout(2000);
 
   await page.getByRole('button', { name: 'Login' }).click();
@@ -14,51 +14,20 @@ async function handleLogin(page) {
 }
 
 test('login', async ({ page }) => {
-  await page.goto('https://ambitious-flower-079bde70f.5.azurestaticapps.net/');
+  await page.goto('http://localhost:3030');
   
   await handleLogin(page);
-
   await page.waitForTimeout(3000);
 
-  await page.getByText('Activos').click();
-
+  await page.getByText('Categorías').click();
   await page.waitForTimeout(2000);
 
-  await page.getByRole('link', { name: 'Nuevo Activo' }).click();
-
+  await page.getByTestId('KeyboardArrowRightIcon').click();
   await page.waitForTimeout(2000);
 
-  await page.getByRole('combobox', { name: 'Tipo' }).fill('Aire Acondicionado');
-
-  await page.getByRole('option', { name: 'Aire Acondicionado' }).click();
-
+  await page.getByTestId('KeyboardArrowRightIcon').click();
   await page.waitForTimeout(2000);
 
-  await page.getByRole('textbox', { name: 'Nombre' }).fill('Activo Prueba');
-
-  await page.waitForTimeout(1000);
-
-  await page.locator('.ql-editor').fill('Nota Prueba 0303');
-
-  await page.waitForTimeout(2000);
-
-  await page.getByRole('textbox', { name: 'Clave' }).fill('030303');
-
-  await page.waitForTimeout(2000);
-
-  const dateField = page.locator('input[placeholder="DD/MM/YY hh:mm"]');
-  await dateField.click({ position: { x: 10, y: 10 } });
-  await page.keyboard.type('0611250025');
-
-  await page.waitForTimeout(2000);
-
-  await page.locator('input[value="ARGENTINA"]').click();
-  await page.waitForTimeout(1000);
-
-  await page.getByRole('textbox', { name: 'Buscar' }).fill('Almacen Central Bs As');
-  await page.waitForTimeout(1000);
-
-  await page.getByText('Almacen Central Bs As').click();
-
+  await page.getByText('Autorización').click();
   await page.waitForTimeout(2000);
 });
