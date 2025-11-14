c09bda2e443f1d814133348f31dabd471e30e30f
Martin Backhaus <tinchoignacio33@gmail.com>
2025-11-14 12:53:13 -0300

    usuarioz actualizado

--- tests/UsuarioZ.spec.js
+++ tests/UsuarioZ.spec.js
@@ -3,30 +3,39 @@ import { test, expect } from '@playwright/test';
 test.setTimeout(120000);
 
 async function handleLogin(page) {
-  await page.getByLabel('Email').fill('claims@consuman.com');
+  await page.getByLabel('Email').fill('admin@consuman.com');
   await page.getByLabel('Password').fill('123');
   await page.getByRole('button', { name: 'Login' }).click();
 }
 
 test('login', async ({ page }) => {
-    await page.goto('http://localhost:3030');
-    await handleLogin(page);
-    await page.getByText('Solicitudes De Trabajo').click()
-    await page.getByRole('button', { name: /^Crear$/ }).click();
-    await page.locator('input[name="title"]').fill('Solicitud Prueba');
-    await page.waitForTimeout(2000); 
+  await page.goto('http://localhost:3030');
+  await handleLogin(page);
+  await page.getByText('Solicitudes De Trabajo').click()
+  await page.getByRole('button', { name: /^Crear$/ }).click();
+  await page.locator('input[name="title"]').fill('Solicitud Prueba');
+  await page.waitForTimeout(2000); 
+  // Abrí el campo "Activo"
+  await page.getByRole('combobox', { name: /^Activo$/ }).click() ;
 
-    await page.getByRole('combobox', { name: /Activos/i }).click();
-    await page.locator('label:has-text("Activos")').locator('..').getByRole('combobox').click();
+  // En el panel lateral, buscá y elegí ACTIVO_NOMBRE
+  const panel = page.locator('.dx-overlay-content, .MuiDrawer-root').last();
+  await panel.locator('input[aria-label="Buscar"]').fill('ACTIVO_NOMBRE');
+  await page.getByText('ACTIVO_NOMBRE').click();
+  await page.waitForTimeout(2000);
 
-    await page.waitForTimeout(2000); 
-    const buscador = page.locator('input.dx-texteditor-input[aria-label="Buscar"][role="textbox"]').first();
-    await page.waitForTimeout(2000); 
-    await buscador.waitFor({ state: 'visible' });
-    await buscador.fill('ACTIVO_NOMBRE');
-    await page.waitForTimeout(2000); 
+  // (si pide confirmar la selección)
+  //await panel.getByRole('button', { name: /Guardar cambios/ }).first().click().catch(() => {});
 
-    await page.locator('[role="listbox"] [role="option"]').first().click();
-    await page.waitForTimeout(2000); 
 
+  const fecha = page.getByPlaceholder('DD/MM/YYYY');
+  await fecha.waitFor({ state: 'visible' });
+  await fecha.click();
+  await page.waitForTimeout(1000);
+  await fecha.type('14112025');
+
+
+  
+  await page.getByText('Crear solicitud de trabajo').click();
+  await page.waitForTimeout(5000);
 });
